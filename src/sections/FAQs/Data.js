3b58c3dc19e537c16f2fc16ab87437d449860406
micro-prog <sajjadsttr01@gmail.com>
2023-05-10 17:43:01 +0330

    update

---
+++ src/sections/FAQs/Data.js
@@ -0,0 +1,21 @@
+const Data = [
+    {
+        id: 1,
+        question: 'this is question one.',
+        answer: 'this is answer to question one.'
+    },
+
+    {
+        id: 2,
+        question: 'this is question two.',
+        answer: 'this is answer to question two.'
+    },
+
+    {
+        id: 3,
+        question: 'this is question three.',
+        answer: 'this is answer to question three.'
+    }
+]
+
+export default Data;
