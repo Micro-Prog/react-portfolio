6ff21be9d7d2bf08c9b21844b6ba3d1b4869f5bb
Micro-Prog <sajjadsttr01@gmail.com>
2023-05-12 21:38:50 +0330

    update

--- src/sections/contact/Contact.jsx
+++ src/sections/contact/Contact.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import './contact.css'
+import Data from './Data'
 
 const Contact = () => {
   return (
     <section id="contact">
      
-      <h2>Contact</h2>
+      <h2>Get in Touch</h2>
+      <p>Send Me a Message.</p>
+      
+      <div className="contact__icons">
+        {
+          Data.map(item => (
+            <a key={item.id} href={item.link}>{item.icon}</a>
+          ))
+        }
+      </div>
 
     </section>
   )
---
+++ src/sections/contact/Data.js
@@ -0,0 +1,11 @@
+import { AiFillGithub, AiOutlineWhatsApp } from "react-icons/ai";
+import { TbBrandGmail } from "react-icons/tb";
+
+const Data = [
+
+    {id: 1, link: 'aaa@gmail.com', icon: <TbBrandGmail />},
+    {id: 2, link: 'aaa@whatsapp.com', icon: <AiOutlineWhatsApp />},
+    {id: 3, link: 'aaa@github.com', icon: <AiFillGithub />}
+]
+
+export default Data;
